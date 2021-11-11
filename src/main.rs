884d8c0301ea178953f8b7e37b15a78728c13bf1
ken <ken@hellotalk.com>
2021-11-11 19:51:11 +0800

    tokio instead of async-std

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 #![recursion_limit = "256"]
 
+use std::error::Error;
+use std::net::SocketAddr;
 use std::os::raw::{c_char, c_int, c_short, c_uchar, c_ulong, c_ushort};
+use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::sync::Arc;
 
 use libc::*;
 use nix::sys::socket::InetAddr;
 
-use async_std::{
-    fs::OpenOptions,
-    io::{self, stdin, BufReader, BufWriter},
-    os::unix::io::AsRawFd,
-    prelude::*,
-    task,
+use tokio::io::AsyncBufReadExt;
+use tokio::{
+    fs::File,
+    io::{stdin, AsyncReadExt, AsyncWriteExt, BufReader, BufWriter},
     net::UdpSocket,
+    select,
+    // sync::mpsc,
 };
-use futures::{future::FutureExt, select};
 
 macro_rules! ioctl(
 	($fd:expr, $flags:expr, $value:expr) => ({
@@ -97,98 +100,179 @@ const SIOCSIFFLAGS: u64 = 0x8914; /* set flags */
 const SIOCSIFADDR: u64 = 0x8916; /* set PA address */
 const SIOCSIFNETMASK: u64 = 0x891c; /* set network PA mask */
 
-fn main() -> io::Result<()> {
-    task::block_on(async {
-        let tun_file = OpenOptions::new()
-            .read(true)
-            .append(true)
-            .open("/dev/net/tun")
-            .await?;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let tun_file = File::open("/dev/net/tun").await?;
+    let rawfd = tun_file.as_raw_fd();
 
-        // iface up
-        let mut req = ifreq::with_if_name("");
-        req.ifr_ifru.ifr_flags = IFF_TUN | IFF_NO_PI | IFF_MULTI_QUEUE;
-        unsafe { ioctl!(tun_file.as_raw_fd(), TUNSETIFF, &req) }?;
+    // iface up
+    let mut req = ifreq::with_if_name("");
+    req.ifr_ifru.ifr_flags = IFF_TUN | IFF_NO_PI | IFF_MULTI_QUEUE;
+    unsafe { ioctl!(rawfd, TUNSETIFF, &req) }?;
 
-        // set ip
-        const IPPROTO_IP: c_int = 0;
-        let sock4 = unsafe { socket(AF_INET, SOCK_DGRAM, IPPROTO_IP) };
-        unsafe {
-            ioctl!(sock4, SIOCGIFFLAGS, &req)?;
-            req.ifr_ifru.ifr_flags |= IFF_UP | IFF_RUNNING;
-            ioctl!(sock4, SIOCSIFFLAGS, &req)?;
+    // set ip
+    const IPPROTO_IP: c_int = 0;
+    let sock4 = unsafe { socket(AF_INET, SOCK_DGRAM, IPPROTO_IP) };
+    unsafe {
+        ioctl!(sock4, SIOCGIFFLAGS, &req)?;
+        req.ifr_ifru.ifr_flags |= IFF_UP | IFF_RUNNING;
+        ioctl!(sock4, SIOCSIFFLAGS, &req)?;
+    }
+
+    let cidr: ipnet::IpNet = "10.0.5.1/24".parse().unwrap();
+    let addr = InetAddr::from_std(&(cidr.addr(), 0).into());
+    match addr {
+        InetAddr::V4(sockaddr_in) => unsafe {
+            req.ifr_ifru.ifr_addr = std::mem::transmute(sockaddr_in);
+            ioctl!(sock4, SIOCSIFADDR, &req)?;
+        },
+        InetAddr::V6(_) => {}
+    };
+
+    // set mask
+    let netmask = InetAddr::from_std(&(cidr.netmask(), 0).into());
+    match netmask {
+        InetAddr::V4(sockaddr_in) => unsafe {
+            req.ifr_ifru.ifr_netmask = std::mem::transmute(sockaddr_in);
+            ioctl!(sock4, SIOCSIFNETMASK, &req)?;
+        },
+        InetAddr::V6(_) => (),
+    };
+
+    let mut tun_reader = BufReader::new(unsafe { File::from_raw_fd(rawfd) });
+    let mut tun_writer = BufWriter::new(unsafe { File::from_raw_fd(rawfd) });
+    // let mut stdin_reader = BufReader::new(stdin());
+
+    let udp_socket = UdpSocket::bind("0.0.0.0:9090").await?;
+    println!("Listening on {}", udp_socket.local_addr()?);
+    let udp_receiver = Arc::new(udp_socket);
+    let udp_sender = udp_receiver.clone();
+    // let (tx, mut rx) = mpsc::channel::<(Vec<u8>, SocketAddr)>(1_000);
+
+    // udp send
+    /*tokio::spawn(async move {
+        while let Some((data, addr)) = rx.recv().await {
+            match udp_sender.send_to(&data, &addr).await {
+                Ok(n) => {
+                    println!("{:?} bytes sent", n);
+                }
+                Err(e) => {
+                    println!("udp read error: {}", e);
+                }
+            }
         }
+    });*/
 
-        let cidr: ipnet::IpNet = "10.0.5.1/24".parse().unwrap();
-        let addr = InetAddr::from_std(&(cidr.addr(), 0).into());
-        match addr {
-            InetAddr::V4(sockaddr_in) => unsafe {
-                req.ifr_ifru.ifr_addr = std::mem::transmute(sockaddr_in);
-                ioctl!(sock4, SIOCSIFADDR, &req)?;
-            },
-            InetAddr::V6(_) => {}
-        };
-
-        // set mask
-        let netmask = InetAddr::from_std(&(cidr.netmask(), 0).into());
-        match netmask {
-            InetAddr::V4(sockaddr_in) => unsafe {
-                req.ifr_ifru.ifr_netmask = std::mem::transmute(sockaddr_in);
-                ioctl!(sock4, SIOCSIFNETMASK, &req)?;
-            },
-            InetAddr::V6(_) => (),
-        };
-
-        let stdin = BufReader::new(stdin());
-        let mut lines_from_stdin = futures::StreamExt::fuse(stdin.lines());
-        let mut reader = BufReader::new(&tun_file);
-        let mut writer = BufWriter::new(&tun_file);
-
-        let mut tun_buf = vec![0u8; 1500];
-
-        let socket = UdpSocket::bind("0.0.0.0:9090").await?;
-
-        // let mut buf = vec![0u8; 1024];
-        println!("Listening on {}", socket.local_addr()?);
-        // let mut lines_from_server = BufReader::new(reader).lines().fuse();
-
-        loop {
-            select! {
-                r = reader.read(&mut tun_buf).fuse() => match r {
-                    Ok(n) => {
+    // udp receive
+    tokio::spawn(
+        async move {
+            loop {
+                let mut udp_buf = vec![0u8; 1024];
+                match udp_receiver.recv_from(&mut udp_buf).await  {
+                    Ok((n, peer)) => {
                         if n > 0 {
-                            println!("read {} bytes {:?} from tun", n, &tun_buf[..n]);
+                            println!("received {} bytes {:?} from {}", n, &udp_buf[..n], peer);
                         }
-                    }
-                    Err(e) => {
-                        println!("read error: {}", e);
-                    }
-                },
-                line = lines_from_stdin.next().fuse() => match line {
-                    Some(line) => {
-                        println!("line: {}", line?);
-                        // let buf: [u8; 32] = [0x45, 0x00, 0x00, 0x20, 0x91, 0xb3, 0x40, 0x00, 0x40, 0x11, 0x8d, 0x17, 0x0a, 0x00, 0x05, 0x01, 0x0a, 0x00, 0x05, 0x03, 0x83, 0x0d, 0x1f, 0x90, 0x00, 0x0c, 0x7c, 0xc9, 0x61, 0x62, 0x63, 0x0a];
-                        let buf: [u8; 84] = [69, 0, 0, 84, 97, 87, 64, 0, 64, 1, 187, 79, 10, 0, 5, 1, 10, 0, 5, 2, 8, 0, 45, 248, 90, 168, 0, 1, 23, 64, 70, 96, 0, 0, 0, 0, 70, 235, 12, 0, 0, 0, 0, 0, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 5];
-                        match writer.write(&buf).await { // block here, not writable
+                        match tun_writer.write(&udp_buf[..n]).await {
                             Ok(n) => {
                                 println!("write {} bytes to tun", n);
                             },
                             Err(e) => {
-                                println!("write error: {}", e);
+                                println!("tun write error: {}", e);
                             }
                         }
-                        // the codes below works fine.
-                        // let n = match unsafe { libc::write(tun_file.as_raw_fd(), buf.as_ptr() as _, buf.len() as _) } {
-                        //     -1 => 0,
-                        //     n => n,
-                        // };
-                        // println!("write {} bytes to tun", n);
                     }
-                    None => break,
-                },
+                    Err(e) => {
+                        println!("udp read error: {}", e);
+                    }
+                }
             }
         }
+    );
 
-        Ok(())
-    })
+    // stdin read
+    /*tokio::spawn(async move {
+        loop {
+            let mut stdin_buf = String::new();
+            match stdin_reader.read_line(&mut stdin_buf).await {
+                Ok(n) => {
+                    if n > 0 {
+                        println!("read {} bytes {:?} from stdin", n, stdin_buf);
+                    }
+                    let buf: [u8; 84] = [
+                        69, 0, 0, 84, 97, 87, 64, 0, 64, 1, 187, 79, 10, 0, 5, 1, 10, 0, 5, 2, 8,
+                        0, 45, 248, 90, 168, 0, 1, 23, 64, 70, 96, 0, 0, 0, 0, 70, 235, 12, 0, 0,
+                        0, 0, 0, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+                        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+                        51, 52, 53, 54, 5,
+                    ];
+                    match tun_writer.write(&buf).await {
+                        Ok(n) => {
+                            println!("write {} bytes to tun", n);
+                        }
+                        Err(e) => {
+                            println!("tun write error: {}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("stdin read error: {}", e);
+                }
+            }
+        }
+    });*/
+
+    loop {
+        let mut tun_buf = vec![0u8; 1500];
+        // let mut stdin_buf = String::new();
+
+        select! {
+            r = tun_reader.read(&mut tun_buf) => match r {
+                Ok(n) => {
+                    if n > 0 {
+                        println!("read {} bytes {:?} from tun", n, &tun_buf[..n]);
+                    }
+                    let remote_addr:SocketAddr = "127.0.0.1:9091".parse().unwrap();
+                    /*match tx.send((tun_buf, remote_addr)).await {
+                        Ok(()) => {
+                        }
+                        Err(e) => {
+                            println!("channel send error: {}", e);
+                        }
+                    }*/
+                    match udp_sender.send_to(&tun_buf[..n], &remote_addr).await {
+                        Ok(n) => {
+                            println!("{:?} bytes sent", n);
+                        }
+                        Err(e) => {
+                            println!("udp read error: {}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("tun read error: {}", e);
+                }
+            },
+
+            /*r = stdin_reader.read_line(&mut stdin_buf) => match r {
+                Ok(n) => {
+                    if n > 0 {
+                        println!("read {} bytes {:?} from stdin", n, stdin_buf);
+                    }
+                    let buf: [u8; 84] = [69, 0, 0, 84, 97, 87, 64, 0, 64, 1, 187, 79, 10, 0, 5, 1, 10, 0, 5, 2, 8, 0, 45, 248, 90, 168, 0, 1, 23, 64, 70, 96, 0, 0, 0, 0, 70, 235, 12, 0, 0, 0, 0, 0, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 5];
+                    match tun_writer.write(&buf).await {
+                        Ok(n) => {
+                            println!("write {} bytes to tun", n);
+                        },
+                        Err(e) => {
+                            println!("tun write error: {}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("stdin read error: {}", e);
+                }
+            },*/
+        }
+    }
 }
